2819fdffb32e0c5db7cfb30b0aa7a02dc3bec5cb
Oscar Sandoval <Ossagar10@gmail.com>
2026-03-14 14:53:21 -0600

    Commit 4

--- Leetcode/src/main/java/umg/proyecto/parcial1/Leetcode/Ejercicio2.java
+++ Leetcode/src/main/java/umg/proyecto/parcial1/Leetcode/Ejercicio2.java
@@ -5,19 +5,41 @@ import java.util.Arrays;
 public class Ejercicio2 {
 
     public static void main(String[] args) {
-
-        int[] datos = {7, 2, 9, 4, 1, 8};
+        int[] datos = {4, 3, 7, 9, 2, 6};
         
         int[] resultado = secondMinMax(datos);
         
-        System.out.println("Entrada: [7,2,9,4,1,8]");
-        System.out.println("Resultado: " + Arrays.toString(resultado));
         
+        System.out.println("Entrada: [4, 3, 7, 9, 2, 6]");
+        System.out.println("Resultado: " + Arrays.toString(resultado));
     }
+
+  
     public static int[] secondMinMax(int[] numbers) {
+        
         int max = Integer.MIN_VALUE;
         int secondMax = Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;
         int secondMin = Integer.MAX_VALUE;
-}
+
+        for (int num : numbers) {
+           
+            if (num > max) {
+                secondMax = max;
+                max = num;
+            } else if (num > secondMax && num != max) {
+                secondMax = num;
+            }
+
+         
+            if (num < min) {
+                secondMin = min;
+                min = num;
+            } else if (num < secondMin && num != min) {
+                secondMin = num;
+            }
+        }
+
+        return new int[]{secondMin, secondMax};
+    }
 }
