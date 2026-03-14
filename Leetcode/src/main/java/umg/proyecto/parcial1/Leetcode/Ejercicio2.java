549b07962d8c2176b093b3a5008e9869f9600f90
Oscar Sandoval <Ossagar10@gmail.com>
2026-03-14 14:34:51 -0600

    Commit 2

---
+++ Leetcode/src/main/java/umg/proyecto/parcial1/Leetcode/Ejercicio2.java
@@ -0,0 +1,13 @@
+package umg.proyecto.parcial1.Leetcode;
+
+import java.util.Arrays;
+
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+
+        int[] datos = {7, 2, 9, 4, 1, 8};
+        
+        int[] resultado = secondMinMax(datos);
+    }
+}
